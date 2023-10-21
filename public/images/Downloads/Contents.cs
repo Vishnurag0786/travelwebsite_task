183ee00193046d5a2c6865a15c17e026a542cdb4
Vishnurag N U <vishnurag9740@gmail.com>
2023-10-21 20:17:03 +0530

    first commit

---
+++ public/images/Downloads/Contents.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Prorata_Main_Web.Models
+{
+    public static class Contents
+    {
+        public static string INTRODUCTION_SMS_FUNNEL_TEMPLATE_ID = "1707166452526835368";
+        public static string INTRODUCTION_SMS_FUNNEL = "Thank you for filing in your enquiry. Our team will get in touch with you when we are live in your society / city.\n- Team Prorata";
+  
+    }
+}
